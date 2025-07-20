0411af200bef3c445654b11cf3ca15f7b2b5b38c
tksimeji <tksimeji@outlook.com>
2025-07-20 11:45:28 +0900

    feat: implemented the nyansole

--- src/components/hero/Terminal.tsx
+++ src/components/hero/Terminal.tsx
@@ -1,34 +1,30 @@
 'use client';
 
-import React, { JSX, useState } from 'react';
+import React, { useState } from 'react';
 import TypewriterText from '@/components/TypewriterText';
 import { sourceCodePro } from '@/app/fonts';
+import Image from 'next/image';
 
-const out: JSX.Element[] = [
-];
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search');
+  const result = await res.json();
+  return result[0];
+};
 
+interface SearchCatImage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
 
 const Terminal = () => {
-  const [lines, setLines] = useState<JSX.Element[]>([]);
+  const [catImageUrl, setCatImageUrl] = useState('');
 
-  const handleCommandTypingComplete = () => {
-    let currentIndex = 0;
-
-    const appendInterval = setInterval(() => {
-      if (currentIndex < out.length) {
-        const uniqueKey = `${currentIndex}-${Date.now()}`;
-        const element = React.cloneElement(out[currentIndex], {
-          key: uniqueKey
-        });
-
-        setLines((prev) => [...prev, element]);
-
-        currentIndex++;
-      } else {
-        clearInterval(appendInterval);
-      }
-    }, 100);
-  }
+  const handleCommandTypingComplete = async () => {
+    const catImage = await fetchCatImage();
+    setCatImageUrl(catImage.url)
+  };
 
   return (
     <>
@@ -39,13 +35,16 @@ const Terminal = () => {
           <div className="bg-[#28c941] h-[16px] rounded-full w-[16px]" />
         </div>
         <div className="absolute font-medium left-1/2 text-amber-50 transform -translate-x-1/2">
-          tksimeji@vmxxx
+          Nyansole
         </div>
       </div>
       <div className={`bg-black h-[360px] p-4 rounded-b-xl ${sourceCodePro.className} text-white`}>
-        <TypewriterText content="cat /etc/os-release" prefix="[tksimeji@saba ~]$ " callback={handleCommandTypingComplete} />
-        {lines}
+        <TypewriterText content="cat /etc/os-release" prefix="$ " callback={handleCommandTypingComplete} />
+        <div className="h-[280px] my-4 relative w-full">
+          {catImageUrl && (<Image className="object-contain" alt="Cat" fill src={catImageUrl} />)}
+        </div>
       </div>
+      <p className="text-secondary-text">Powered by <a href="https://thecatapi.com">The Cat API</a></p>
     </>
   );
 }
